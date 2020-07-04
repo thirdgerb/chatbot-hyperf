a8c3f6eb378afc69eb3bf6328b0d0f30c4e36c16
thirdgerb <thirdgerb@gmail.com>
2020-07-05 05:26:58 +0800

    add migrations for hyperf

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -15,6 +15,26 @@ class ConfigProvider
                 StartAppCommand::class,
             ],
 
+            'publish' => [
+                [
+                    'id' => 'memories table',
+                    'description' => 'migrations for memories',
+                    'source' => __DIR__ . '/../publish/migraitons/2020_07_05_0_create_memories_table.php',
+                    'destination' => BASE_PATH . '/migrations/2020_07_05_0_create_memories_table.php'
+                ],
+                [
+                    'id' => 'messages table',
+                    'description' => 'migration for messages',
+                    'source' => __DIR__ . '/../publish/migraitons/2020_07_05_0_create_messages_table.php',
+                    'destination' => BASE_PATH . '/migrations/2020_07_05_0_create_messages_table.php'
+                ],
+                [
+                    'id' => 'options table',
+                    'description' => 'migration for options',
+                    'source' => __DIR__ . '/../publish/migraitons/2020_07_05_0_create_options_table.php',
+                    'destination' => BASE_PATH . '/migrations/2020_07_05_0_create_options_table.php'
+                ],
+            ],
         ];
     }
 
